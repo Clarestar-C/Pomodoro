d6b96611c3a94fe3bb3d68b5475fe972fd85607d
Claresta <clarestacheung@gmail.com>
2026-08-19 17:52:47 -0400

    Refactor and restore script.js structure

--- script.js
+++ script.js
@@ -199,7 +199,7 @@ function playAmbientSound() {
     ambientAudioA.volume = volume;
     ambientAudioB.volume = volume;
 
-    const bufferTime = 2.0; // Seconds before the end to trigger the next track
+    const bufferTime = 4.0; // Seconds before the end to trigger the next track
 
     function attachLoopListener(currentDeck, nextDeck) {
         currentDeck.ontimeupdate = function() {
