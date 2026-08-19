8865fb119518cdff57616617ec0447a53b78e02d
Claresta <clarestacheung@gmail.com>
2026-08-19 15:33:22 -0400

    Implement service worker for offline caching
    This service worker caches static assets for offline use and manages cache updates.

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'pomodorodoro-v1';
+
+// List all the static assets your app needs to run offline
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './style.css',
+  './pfp.jpeg',
+  './manifest.json'
+  // Add any extra local .js files or sound files here (e.g., './script.js', './alarm.mp3')
+];
+
+// Install Event: Caches all static resources
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Activate Event: Cleans up old cache versions if you update CACHE_NAME
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch Event: Serves resources from cache when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
